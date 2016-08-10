411d5304c65f14195323c459e517c5856005ce0b
tiagoadami <tiagoadami@192.168.56.1>
2016-08-10 18:52:01 -0300

    Minor changes
    Not worth mention

--- Utils/src/com/adamiworks/utils/ClassUtils.java
+++ Utils/src/com/adamiworks/utils/ClassUtils.java
@@ -14,7 +14,7 @@ import java.util.jar.JarFile;
 
 import sun.net.www.protocol.file.FileURLConnection;
 
-@SuppressWarnings("restriction")
+//@SuppressWarnings("restriction")
 public class ClassUtils {
 	/**
 	 * Private helper method
