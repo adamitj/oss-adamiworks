c1521863b86456c928ce4553da55bb095b78ad68
Adami <adami@powerdba.com.br>
2022-06-21 15:16:31 -0300

    Changes on CryptoUtils

--- Utils/src/com/adamiworks/utils/TestApp.java
+++ Utils/src/com/adamiworks/utils/TestApp.java
@@ -1,13 +1,35 @@
 package com.adamiworks.utils;
 
+import com.adamiworks.utils.crypto.CryptoMode;
+import com.adamiworks.utils.crypto.CryptoUtils;
+
 public class TestApp {
 
 	public static void main(String[] args) {
-		System.out.println(StringUtils.left("Tiago", 4));
+		String texto = "Tiago José Adami, Analice Cristina Lewinski Lopes, Laura Rafaela Lopes Adami";
+		String password = "IdontKnow0ClowsG1v3meFre4AdkingNootZBicausiTheyA$%0)sdjneifh";
 
-		System.out.println(StringUtils.right("Tiago", 3));
+		try {
+			CryptoUtils aes = new CryptoUtils(CryptoMode.AES, password);
+			CryptoUtils des = new CryptoUtils(CryptoMode.DES, password);
 
-		System.out.println(StringUtils.capitalize("tiago"));
+			System.out.println(CryptoUtils.getHash8String(password));
+			System.out.println(CryptoUtils.getHash16String(password));
+
+			String eAes = aes.encrypt(texto);
+			String eDes = des.encrypt(texto);
+			
+			System.out.println(eAes);
+			System.out.println(eDes);
+			
+			System.out.println(aes.decrypt(eAes));
+			System.out.println(des.decrypt(eDes));
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
--- Utils/src/com/adamiworks/utils/crypto/CryptoUtils.java
+++ Utils/src/com/adamiworks/utils/crypto/CryptoUtils.java
@@ -3,6 +3,7 @@ package com.adamiworks.utils.crypto;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Random;
@@ -14,8 +15,12 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-//import org.apache.tomcat.util.codec.binary.Base64;
-
+/**
+ * Class that abstract common DES and AES encription functions.
+ * 
+ * @author adami
+ *
+ */
 public class CryptoUtils {
 
 	private byte[] keyBytes;
@@ -23,47 +28,45 @@ public class CryptoUtils {
 	private CryptoMode mode;
 
 	/**
-	 * Creates a new CryptoUtil object to encode and decode Strings with AES or
-	 * DES converting them into/from a Base64 readable String.
+	 * Creates a new CryptoUtil object to encode and decode Strings with AES or DES
+	 * converting them into/from a Base64 readable String.
 	 * 
-	 * Notice that AES uses 128-bit keys and DES uses 56-bit keys (+1 bit for
-	 * parity each byte).
-	 * 
-	 * That means you need to use a <b>16 length key for AES</b> and <b>8 length
-	 * key for DES.</b>
+	 * This constructor initialize the object using a given password encryption
+	 * String of your choice.
 	 * 
 	 * @param mode
-	 * @param symetricKey
+	 * @param passwordKey
 	 * @throws InvalidKeyException
 	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
 	 */
-	public CryptoUtils(CryptoMode mode, String symetricKey) throws InvalidKeyException, NoSuchAlgorithmException {
-		this.mode = mode;
-		keyBytes = symetricKey.getBytes();
+	public CryptoUtils(CryptoMode mode, String passwordKey)
+			throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		byte keyBytes[] = null;
 
 		switch (mode) {
-		case AES:
-			internalMode = "AES/CBC/PKCS5Padding";
-			break;
-
-		case DES:
-			internalMode = "DES/CBC/PKCS5Padding";
-			break;
-
-		default:
-			break;
+			case AES:
+				keyBytes = getHash16String(passwordKey).getBytes();
+				break;
+			case DES:
+				keyBytes = getHash8String(passwordKey).getBytes();
+				break;
+			default:
+				break;
 		}
+
+		this.initialize(mode, keyBytes);
 	}
 
 	/**
-	 * Creates a new CryptoUtil object to encode and decode Strings with AES or
-	 * DES converting them into/from a Base64 readable String.
+	 * Creates a new CryptoUtil object to encode and decode Strings with AES or DES
+	 * converting them into/from a Base64 readable String.
 	 * 
-	 * Notice that AES uses 128-bit keys and DES uses 56-bit keys (+1 bit for
-	 * parity each byte).
+	 * Notice that AES uses 128-bit keys and DES uses 56-bit keys (+1 bit for parity
+	 * each byte).
 	 * 
-	 * That means you need to use a <b>16 bytes for AES key</b> and <b>8 bytes
-	 * for DES key</b>.
+	 * That means you need to use a <b>16 bytes for AES key</b> and <b>8 bytes for
+	 * DES key</b>.
 	 * 
 	 * @param mode
 	 * @param symetricKey
@@ -71,21 +74,106 @@ public class CryptoUtils {
 	 * @throws NoSuchAlgorithmException
 	 */
 	public CryptoUtils(CryptoMode mode, byte[] keyBytes) throws InvalidKeyException, NoSuchAlgorithmException {
+		initialize(mode, keyBytes);
+	}
+
+	/**
+	 * Initialize the cripto mode and key
+	 * 
+	 * @param mode
+	 * @param keyBytes
+	 */
+	private void initialize(CryptoMode mode, byte[] keyBytes) {
 		this.mode = mode;
 		this.keyBytes = keyBytes;
 
 		switch (mode) {
-		case AES:
-			internalMode = "AES/CBC/PKCS5Padding";
-			break;
+			case AES:
+				internalMode = "AES/CBC/PKCS5Padding";
+				break;
 
-		case DES:
-			internalMode = "DES/CBC/PKCS5Padding";
-			break;
+			case DES:
+				internalMode = "DES/CBC/PKCS5Padding";
+				break;
 
-		default:
-			break;
+			default:
+				break;
 		}
+	}
+
+	/**
+	 * Gets a MD5 hash value for a given text.
+	 * 
+	 * @param text
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 */
+	private static String getMD5(String arg) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		byte[] bytesOfMessage = arg.getBytes("UTF-8");
+
+		MessageDigest md;
+		md = MessageDigest.getInstance("MD5");
+		byte[] thedigest = md.digest(bytesOfMessage);
+
+		StringBuffer hexString = new StringBuffer();
+		for (int i = 0; i < thedigest.length; i++) {
+			String hex = Integer.toHexString(0xff & thedigest[i]);
+			if (hex.length() == 1)
+				hexString.append('0');
+			hexString.append(hex);
+		}
+		return hexString.toString();
+	}
+
+	/**
+	 * Generates a 16 char string hash from a given String text. <b>This is a custom
+	 * application implementation. Not following any standard.</b>
+	 * 
+	 * @param text The plain string to get the 16 hash value based on MD5.
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String getHash16String(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		String md5hash = getMD5(text);
+
+		// Get the first char from the md5hash
+		String hash16 = md5hash.substring(0, 1);
+
+		// Will follow to get remaining 15 chars only
+		for (int i = 0; i < 15; i++) {
+			// Hash from the composite existing hash16 + given String + the current
+			// iteration, but gets only the first char to add.
+			hash16 += (getMD5(hash16 + md5hash + String.valueOf(i+1))).substring(0, 1);
+		}
+
+		return hash16;
+	}
+
+	/**
+	 * Generates a 8 char string hash from a given String text. <b>This is a custom
+	 * application implementation. Not following any standard.</b>
+	 * 
+	 * @param text The plain string to get the 16 hash value based on MD5.
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String getHash8String(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		String md5hash = getMD5(text);
+
+		// Get the second char from the md5hash
+		String hash8 = md5hash.substring(1, 2);
+
+		// Will follow to get remaining 8 chars only
+		for (int i = 0; i < 7; i++) {
+			// Hash from the composite existing hash8 + given String + the current
+			// iteration
+			hash8 += (getMD5(hash8 + md5hash + String.valueOf(i+2))).substring(0,1);
+		}
+
+		return hash8;
 	}
 
 	/**
@@ -98,22 +186,22 @@ public class CryptoUtils {
 	 * @throws InvalidKeyException
 	 * @throws InvalidAlgorithmParameterException
 	 */
-	private Cipher getCipher(byte[] iv, int cipherMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-			InvalidAlgorithmParameterException {
+	private Cipher getCipher(byte[] iv, int cipherMode) throws NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, InvalidAlgorithmParameterException {
 		Cipher cipher = Cipher.getInstance(internalMode);
 		SecretKeySpec keySpec = null;
 
 		IvParameterSpec ivspec = new IvParameterSpec(iv);
 
 		switch (mode) {
-		case AES:
-			keySpec = new SecretKeySpec(keyBytes, "AES");
-			break;
-		case DES:
-			keySpec = new SecretKeySpec(keyBytes, "DES");
-			break;
-		default:
-			break;
+			case AES:
+				keySpec = new SecretKeySpec(keyBytes, "AES");
+				break;
+			case DES:
+				keySpec = new SecretKeySpec(keyBytes, "DES");
+				break;
+			default:
+				break;
 		}
 
 		cipher = Cipher.getInstance(internalMode);
@@ -135,7 +223,8 @@ public class CryptoUtils {
 	 * @throws UnsupportedEncodingException
 	 * @throws InvalidAlgorithmParameterException
 	 */
-	public String encrypt(String text) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+	public String encrypt(String text)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
 			BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
 		byte[] input = text.getBytes("UTF-8");
 		byte[] output = null;
@@ -143,14 +232,14 @@ public class CryptoUtils {
 
 		byte[] iv = null;
 		switch (mode) {
-		case AES:
-			offset = 16;
-			break;
-		case DES:
-			offset = 8;
-			break;
-		default:
-			break;
+			case AES:
+				offset = 16;
+				break;
+			case DES:
+				offset = 8;
+				break;
+			default:
+				break;
 		}
 
 		iv = new byte[offset];
@@ -189,7 +278,8 @@ public class CryptoUtils {
 	 * @throws UnsupportedEncodingException
 	 * @throws InvalidAlgorithmParameterException
 	 */
-	public String decrypt(String text) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+	public String decrypt(String text)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
 			BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
 		byte[] output = null;
 		byte[] input = Base64.getDecoder().decode(text);
@@ -198,14 +288,14 @@ public class CryptoUtils {
 
 		byte[] iv = null;
 		switch (mode) {
-		case AES:
-			offset = 16;
-			break;
-		case DES:
-			offset = 8;
-			break;
-		default:
-			break;
+			case AES:
+				offset = 16;
+				break;
+			case DES:
+				offset = 8;
+				break;
+			default:
+				break;
 		}
 
 		iv = new byte[offset];
